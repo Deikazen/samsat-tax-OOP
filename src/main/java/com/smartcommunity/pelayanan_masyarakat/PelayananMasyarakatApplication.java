9b4f6333a03829281e80d69162af1ccc3596fdd9
Satria <mfsmfsmfs35@gmail.com>
2026-05-21 18:37:10 +0700

    first commit

---
+++ src/main/java/com/smartcommunity/pelayanan_masyarakat/PelayananMasyarakatApplication.java
@@ -0,0 +1,13 @@
+package com.smartcommunity.pelayanan_masyarakat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelayananMasyarakatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelayananMasyarakatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcommunity/pelayanan_masyarakat/PelayananMasyarakatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcommunity.pelayanan_masyarakat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelayananMasyarakatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
